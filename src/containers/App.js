e98fe8484fcb3068d9d54083c3ddf6c21ac1d1cf
Wesley Coetzee <coetzeewr@gmail.com>
2018-08-29 20:20:29 +0200

    File updates

---
+++ src/containers/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="grid-container">
+        <div className="header">Header</div>
+        <div className="games">Menu</div>
+        <div className="details">Main</div>  
+        <div className="footer">Right</div>
+      </div>
+    );
+  }
+}
+
+export default App;
