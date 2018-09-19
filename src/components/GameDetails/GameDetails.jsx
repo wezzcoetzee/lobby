f753e6625935fd431ea49564a0a1acd36669dc77
Wesley <coetzeewr@gmail.com>
2018-09-19 20:49:07 +0200

    Added description to game details Code clean up

--- src/components/GameDetails/GameDetails.jsx
+++ src/components/GameDetails/GameDetails.jsx
@@ -1,23 +1,15 @@
 
 import React from 'react';
-// import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './GameDetails.scss';
 
 class GameDetails extends React.Component {
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     selectedGame: ""
-  //   }
-  // }
-
   render() {
     return (
       <div className="details__wrapper">
         <section>
           <img src={this.props.game.image} alt={this.props.game.name} />
           <h3>{this.props.game.name}</h3>
+          <p>{this.props.game.description}</p>
           <p>Tempreture</p>
           <progress value={this.props.game.heat} max="100"></progress>
         </section>        
--- src/components/Games/Games.jsx
+++ src/components/Games/Games.jsx
@@ -1,6 +1,5 @@
 
 import React from 'react';
-// import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './Games.scss';
 import GameCard from '../GameCard/GameCard';
 
@@ -16,16 +15,6 @@ class Games extends React.Component {
   render() {
     return (
       <div className="games__container">
-        {/* <div className="toggle__button menu--toggle" onClick={() => {this.props.onClick(false)}}>
-          <i className="material-icons">
-            chevron_left
-          </i>
-        </div>
-        <div className="toggle__button scroll--top">
-          <i className="material-icons">
-            expand_less
-          </i>
-        </div> */}
         {this.props.gameList.map((game, i) => <section className="game__container" key={i}><GameCard game={game} onClick={() => {this.props.onClick(true, game)}} /></section>)}
       </div>
     )
